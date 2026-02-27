ab0f75129e317735698b6442dedef5d83a0e1129
Sviat6239 <sviatoslav6239@gmail.com>
2026-02-27 16:27:48 +0100

    added script to navigation between pages

--- static/js/pageRenderer.js
+++ static/js/pageRenderer.js
@@ -1,13 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const projectsDivAsButton = document.querySelectorAll('.projects');
+    const projectsButtons = document.querySelectorAll('.toprojects');
+    const generalButtons = document.querySelectorAll('.togeneral');
     const generalPage = document.querySelector('.card');
+    const projectsPage = document.querySelector('.projects-card');
 
-    projectsDivAsButton.forEach(div => {
-        div.addEventListener('click', () => {
+    projectsPage.classList.add('hidden');
 
+    projectsButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            generalPage.classList.add('hidden');
+            projectsPage.classList.remove('hidden');
         });
     });
 
+    generalButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            projectsPage.classList.add('hidden');
+            generalPage.classList.remove('hidden');
+        });
+    });
 
-})
+});
